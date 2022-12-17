606929e2c5b4d66c1c7a0fad58d4406250ab7192
Emresaridogan <emresaridogan@outlook.com>
2022-12-18 00:46:51 +0300

    model created

--- app/src/main/java/com/emresaridogan/cryptoapi/view/MainActivity.kt
+++ app/src/main/java/com/emresaridogan/cryptoapi/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.emresaridogan.cryptoapi
+package com.emresaridogan.cryptoapi.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emresaridogan.cryptoapi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
